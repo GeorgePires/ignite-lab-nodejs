821498e0031c21763578ef6e239f9e35da917130
GeorgePires <georgeasp.pi@gmail.com>
2022-12-13 18:09:10 -0300

    :sparkles: feat/ add validate notification body

--- src/create-notification-body.ts
+++ src/create-notification-body.ts
@@ -1,9 +1,15 @@
-import { IsNotEmpty, IsUUID } from 'class-validator';
+import { IsNotEmpty, IsUUID, Length } from 'class-validator';
 
 export class CreateNotificationBody {
+  // Attributes validator
   @IsNotEmpty()
   @IsUUID()
   recipientId: string;
+
+  @IsNotEmpty()
+  @Length(5, 240)
   content: string;
+
+  @IsNotEmpty()
   category: string;
 }
