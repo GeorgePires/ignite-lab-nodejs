81b6bc0c3b82b535affa23904920e4cfb23e38fe
GeorgePires <georgeasp.pi@gmail.com>
2022-12-13 18:01:43 -0300

    :sparkles: feat/ create notification body

---
+++ src/create-notification-body.ts
@@ -0,0 +1,9 @@
+import { IsNotEmpty, IsUUID } from 'class-validator';
+
+export class CreateNotificationBody {
+  @IsNotEmpty()
+  @IsUUID()
+  recipientId: string;
+  content: string;
+  category: string;
+}
