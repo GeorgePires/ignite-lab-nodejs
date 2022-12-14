3f9b66ee7bc39872ea443dbdff89f320cd899c76
GeorgePires <georgeasp.pi@gmail.com>
2022-12-14 15:05:46 -0300

    :sparkles: feat/ create http module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { HttpModule } from './http.module';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [AppController],
   providers: [PrismaService],
 })
