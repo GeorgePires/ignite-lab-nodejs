c3d6694ede8c8eac0ba27924e116683784764355
GeorgePires <georgeasp.pi@gmail.com>
2023-01-12 11:57:48 -0300

    :sparkles: feat/ connect notifications-service-kafka

--- src/infra/messaging/kafka/kafka-consumer.service.ts
+++ src/infra/messaging/kafka/kafka-consumer.service.ts
@@ -7,6 +7,22 @@ export class KafkaConsumerService
   implements OnModuleDestroy
 {
   constructor() {
-    super({});
+    super({
+      client: {
+        clientId: 'notifications',
+        brokers: ['handy-mastiff-10419-us1-kafka.upstash.io:9092'],
+        sasl: {
+          mechanism: 'scram-sha-256',
+          username:
+            'aGFuZHktbWFzdGlmZi0xMDQxOSQkKGQWrU-Wcnx5yxClClnorSpO_KvivAWaeAk',
+          password: '5c0939034f7d4d00b2986970ee8b4105',
+        },
+        ssl: true,
+      },
+    });
+  }
+
+  async onModuleDestroy() {
+    await this.close();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
+import { KafkaConsumerService } from '@infra/messaging/kafka/kafka-consumer.service';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions } from '@nestjs/microservices';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
@@ -7,14 +9,22 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
+  const kafkaConsumerService = app.get(KafkaConsumerService);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    strategy: kafkaConsumerService,
+  });
+
+  await app.listen(3000);
+
   const config = new DocumentBuilder()
     .setTitle('Notifications Service')
     .setDescription('Notifications Service - API Documentation')
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
-  await app.listen(3000);
 }
+
 bootstrap();
